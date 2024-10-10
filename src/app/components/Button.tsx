c1da9577b45457d7dd136606eb23075e4f720cb2
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-10 23:20:21 +0300

    add button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,14 +1,24 @@
-import clsx from "clsx";
+import clsx from 'clsx';
 
-type ButtonProps = Partial<React.HTMLProps<HTMLButtonElement>> & {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   disabled?: boolean;
+  children: React.ReactNode;
 };
 
-const Button = ({disabled, ...rest}) => {
-  return <button {...rest} className={clsx("py-2.5 px-5 bg-gray-900 text-zin-50 font-medium text-center rounded",
-    !disabled && "hover:bg-gray-800 active:bg-gray-950",
-    disabled && "text-zinc-100"
-  )}></button>;
+const Button = ({ disabled, children, ...rest }: ButtonProps) => {
+  return (
+    <button
+      {...rest}
+      disabled={disabled}
+      className={clsx(
+        'py-2.5 px-5 bg-gray-900 text-zinc-50 font-medium text-center rounded',
+        !disabled && 'hover:bg-gray-800 active:bg-gray-950',
+        disabled && 'text-zinc-100'
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
