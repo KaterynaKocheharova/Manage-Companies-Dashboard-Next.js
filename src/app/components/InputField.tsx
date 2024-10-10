24e16b8306abb1f709b39cc6c6541be0c38d2cdd
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-10 23:54:56 +0300

    add FieldInput component

---
+++ src/app/components/InputField.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { Field } from 'formik';
+
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  id: string;
+};
+
+const InputField = ({ label, id, ...rest }: InputFieldProps) => {
+  return (
+    <div className="flex flex-column">
+      {label && (
+        <label htmlFor={id} className="text-base text-gray-900 mb-2">
+          {label}
+        </label>
+      )}
+      <Field
+        id={id}
+        {...rest}
+        className="p-3 h-11 text-sm rounded bg-white border border-gray-300 placeholder:text-sm placeholder:text-gray-500 shadow"
+      />
+    </div>
+  );
+};
+
+export default InputField;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', 
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        shadow: '0px 2px 4px -2px #55698705',
+      },
+    },
   },
   plugins: [],
 };
