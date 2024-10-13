6c1faf53c4d6b808c49313fc0de31755186fb33b
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-13 22:28:37 +0300

    add logo uploader

--- src/app/components/LogoUploader.tsx
+++ src/app/components/LogoUploader.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 type LogoUploaderProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
   'type'
@@ -5,7 +7,7 @@ type LogoUploaderProps = Omit<
   label: string;
 };
 
-// to calculate width follow this formula: pixel / 4
+// to calculate follow this formula: pixel / 4
 
 const LogoUploader = ({
   label,
@@ -16,8 +18,25 @@ const LogoUploader = ({
   return (
     <div className="flex gap-5 mb-4">
       {label && <p className="text-base color-gray-900">{label}</p>}
-      <label className="w-44 h-44" htmlFor={id}>
-
+      <label
+        className="w-44 h-44 flex flex-col items-center justify-center bg-white border border border-slate-900 rounded-full cursor-pointer"
+        htmlFor={id}
+      >
+        <Image
+          className="mb-1"
+          alt="upload icon"
+          src="/sprite.svg#icon-cloud-arrow-dwon"
+          width={48}
+          height={48}
+        />
+        {placeholder && <p className="text-base gray-500">{placeholder}</p>}
+        <input
+          id={id}
+          className="hidden"
+          accept="image/*"
+          type="upload"
+          {...rest}
+        />
       </label>
     </div>
   );
