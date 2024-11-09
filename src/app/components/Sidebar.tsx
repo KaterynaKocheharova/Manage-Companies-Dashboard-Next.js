4dd0ea91db84c04e25a3aff358cdb0d7f59c5940
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-10 01:22:26 +0300

    add sidebar and logo

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import Image from 'next/image';
 
 const Sidebar = () => {
   return (
-    <div>
-      SIDEBAR
-    </div>
-  )
-}
+    <aside className="fixed z-50 w-60 h-screen bg-gray-900">
+      <Image
+        alt="logo"
+        width={122}
+        height={24}
+        src="/images/logo.png"
+        className="py-8 mb-11 mx-auto"
+      />
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
