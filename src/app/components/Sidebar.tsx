59671f6ac773594ac2cf896d4fc7299f76a8ffa3
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-11 22:38:57 +0300

    add exit button to sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,29 +4,37 @@ import SidebarItem from './SidebarItem';
 const Sidebar = () => {
   return (
     <aside className="fixed z-50 w-60 h-screen bg-gray-900">
-      <Image
-        alt="logo"
-        width={122}
-        height={24}
-        src="/images/logo.png"
-        className="py-8 mb-11 mx-auto"
-      />
-      <ul>
-        <SidebarItem
-          src="/sprite.svg#icon-squares"
-          alt="dahsboard icon"
-          path="/dashboard"
-        >
-          Dashboard
-        </SidebarItem>
-        <SidebarItem
-          src="/sprite.svg#icon-briefcase"
-          alt="companies icon"
-          path="/companies"
-        >
-          Companies
-        </SidebarItem>
-      </ul>
+      <div className="flex flex-col h-full overflow-y-auto">
+        <Image
+          alt="logo"
+          width={122}
+          height={24}
+          src="/images/logo.png"
+          className="py-8 mb-11 mx-auto"
+        />
+        <ul>
+          <SidebarItem
+            src="/sprite.svg#icon-squares"
+            alt="dahsboard icon"
+            path="/dashboard"
+          >
+            Dashboard
+          </SidebarItem>
+          <SidebarItem
+            src="/sprite.svg#icon-briefcase"
+            alt="companies icon"
+            path="/companies"
+          >
+            Companies
+          </SidebarItem>
+        </ul>
+        <button className="mt-auto flex items-center p-6 mx-auto gap-2 font-medium text-white">
+          <svg width="18" height="18">
+            <use href="/sprite.svg#icon-exit"></use>
+          </svg>{' '}
+          Exit
+        </button>
+      </div>
     </aside>
   );
 };
