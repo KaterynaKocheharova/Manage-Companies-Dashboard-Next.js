30e9aac9de57041affa3c164f0aa86c279513398
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-23 20:58:34 +0300

    add all dashboard components / implement parllel routes

---
+++ src/app/(admin)/dashboard/@sales/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { getSummarySales } from '@/lib/api';
+import SummaryTable from '@/app/components/SummaryTable';
+import SummaryTableHeader from '@/app/components/SummaryTableHeader';
+import SummaryTableCell from '@/app/components/SummaryTableCell';
+import DashboardCard from '@/app/components/DashboardCard';
+
+export default async function page() {
+  const data = await getSummarySales();
+
+  return (
+    <DashboardCard label="Sales details">
+      <SummaryTable
+        headers={
+          <>
+            <SummaryTableHeader>Company</SummaryTableHeader>
+            <SummaryTableHeader align="center">Sold</SummaryTableHeader>
+            <SummaryTableHeader align="center">Income</SummaryTableHeader>
+          </>
+        }
+      >
+        {data.map(({ companyId, companyTitle, sold, income }) => (
+          <tr key={companyId}>
+            <SummaryTableCell>{companyTitle}</SummaryTableCell>
+            <SummaryTableCell align="center">{sold}</SummaryTableCell>
+            <SummaryTableCell align="center">{`$${income}`}</SummaryTableCell>
+          </tr>
+        ))}
+      </SummaryTable>
+    </DashboardCard>
+  );
+}
